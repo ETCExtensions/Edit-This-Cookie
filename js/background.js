566899db0f4f220ce469c4eb6e398ecf5cdc1410
Francesco Capano <fcapano@gmail.com>
2012-12-02 22:14:24 +0100

    added xmas icon

--- js/background.js
+++ js/background.js
@@ -2,9 +2,9 @@ var PAUSED = false;
 var showContextMenu = undefined;
 
 updateCallback = function() {
-    if(showContextMenu != preferences.showContextMenu) {
-    	showContextMenu = preferences.showContextMenu;
-    	setContextMenu(showContextMenu);
+	if(showContextMenu != preferences.showContextMenu) {
+		showContextMenu = preferences.showContextMenu;
+		setContextMenu(showContextMenu);
 	}
 };
 
@@ -20,29 +20,32 @@ var currentVersion = chrome.app.getDetails().version;
 _gaq.push(['_setCustomVar', 3, 'Extension version', currentVersion, 2]);
 if(data.lastVersionRun != currentVersion) {
 	if(data.lastVersionRun == undefined) { //Is firstrun
-	    //chrome.tabs.create({url:chrome.extension.getURL('options_main_page.html')/*+"?page=ads_info"*/}); // ?page=options
-	    chrome.tabs.create({url:chrome.extension.getURL('options_pages/support.html')});
+		//chrome.tabs.create({url:chrome.extension.getURL('options_main_page.html')/*+"?page=ads_info"*/}); // ?page=options
+		chrome.tabs.create({url:chrome.extension.getURL('options_pages/support.html')});
 	}
 	data.lastVersionRun = currentVersion;
 }
 
+nowDate = new Date();
+if( (nowDate.getFullYear() == 2012 && nowDate.getMonth() == 11) || (nowDate.getFullYear() == 2013 && nowDate.getMonth() == 0 && nowDate.getDate() <= 10) ) {
+	chrome.browserAction.setIcon({"path":"/img/cookie_xmas_19x19.png"});
+}
+
 setContextMenu(preferences.showContextMenu);
 
-chrome.cookies.onChanged.addListener(onCookiesChanged);
-
-function onCookiesChanged(changeInfo) {
+chrome.cookies.onChanged.addListener( function(changeInfo) {
 	var removed = changeInfo.removed;
-    var cookie = changeInfo.cookie;
-    var cause = changeInfo.cause;
-    
-    var name = cookie.name;
-    var domain = cookie.domain;
-    var value = cookie.value;
-    
-    if(cause == "expired" || cause == "evicted")
-    	return;
-    
-    console.log("Name: " + name + "\t\tDomain: " + domain + "\t\tCause: " + cause + "\t\tRemoved:" + removed);
+	var cookie = changeInfo.cookie;
+	var cause = changeInfo.cause;
+	
+	var name = cookie.name;
+	var domain = cookie.domain;
+	var value = cookie.value;
+	
+	if(cause == "expired" || cause == "evicted")
+		return;
+	
+	console.log("Name: " + name + "\t\tDomain: " + domain + "\t\tCause: " + cause + "\t\tRemoved:" + removed);
 
 	if(!PAUSED) {
 		
@@ -61,7 +64,7 @@ function onCookiesChanged(changeInfo) {
 						chrome.cookies.set(newCookie);
 						console.log("Cookie Protected! Name:" + name + " / Url:" + newCookie.url + " / Value: " + newCookie.value);
 						++data.nCookiesProtected;
-						return;       
+						return;	   
 					});
 				}
 				return;
@@ -69,37 +72,37 @@ function onCookiesChanged(changeInfo) {
 		}
 		
 		if(!removed) {		//Check if a blocked cookie was added
-		    for(var i=0; i<data.filters.length; i++) {
-		        var currentFilter = data.filters[i];
-		        if(filterMatchesCookie(currentFilter,name,domain,value)) {
-				    chrome.tabs.getSelected(null, function(tab) {
-				        var toRemove = {};
-				        toRemove.url = tab.url;
-				        toRemove.url = "http" + ((cookie.secure) ? "s" : "") + "://" + cookie.domain + cookie.path;
-				        toRemove.name = name;
-				        chrome.cookies.remove(toRemove);
-				        console.log("Cookie Blocked! Name:" + name + " / Url:" + toRemove.url);
-				        ++data.nCookiesFlagged;
-				        return;
-				    });
-			    }
-		    }
+			for(var i=0; i<data.filters.length; i++) {
+				var currentFilter = data.filters[i];
+				if(filterMatchesCookie(currentFilter,name,domain,value)) {
+					chrome.tabs.getSelected(null, function(tab) {
+						var toRemove = {};
+						toRemove.url = tab.url;
+						toRemove.url = "http" + ((cookie.secure) ? "s" : "") + "://" + cookie.domain + cookie.path;
+						toRemove.name = name;
+						chrome.cookies.remove(toRemove);
+						console.log("Cookie Blocked! Name:" + name + " / Url:" + toRemove.url);
+						++data.nCookiesFlagged;
+						return;
+					});
+				}
+			}
 		}
-    }
-    
-    if(!removed && preferences.useMaxCookieAge && preferences.maxCookieAgeType>0) {	//Check expiration, if too far in the future shorten on user's preference
-    	var maxAllowedExpiration = Math.round((new Date).getTime()/1000) + (preferences.maxCookieAge * preferences.maxCookieAgeType);
-    	if(cookie.expirationDate != undefined && cookie.expirationDate > maxAllowedExpiration+60) {
-    		var newCookie = cookieForCreationFromFullCookie(cookie);
-    		if(!cookie.session)
-        		newCookie.expirationDate = maxAllowedExpiration;
+	}
+	
+	if(!removed && preferences.useMaxCookieAge && preferences.maxCookieAgeType>0) {	//Check expiration, if too far in the future shorten on user's preference
+		var maxAllowedExpiration = Math.round((new Date).getTime()/1000) + (preferences.maxCookieAge * preferences.maxCookieAgeType);
+		if(cookie.expirationDate != undefined && cookie.expirationDate > maxAllowedExpiration+60) {
+			var newCookie = cookieForCreationFromFullCookie(cookie);
+			if(!cookie.session)
+				newCookie.expirationDate = maxAllowedExpiration;
 			chrome.cookies.set(newCookie);
-    		console.log("Cookie Shortened! Name:'"+cookie.name+"' from '"+cookie.expirationDate+"' to '"+maxAllowedExpiration+"'");
-    		++data.nCookiesShortened;
-	    	return;
-    	}
-    }
-};
+			console.log("Cookie Shortened! Name:'"+cookie.name+"' from '"+cookie.expirationDate+"' to '"+maxAllowedExpiration+"'");
+			++data.nCookiesShortened;
+			return;
+		}
+	}
+});
 
 /*
 chrome.extension.onMessageExternal.addListener(function(details) {
@@ -108,52 +111,52 @@ chrome.extension.onMessageExternal.addListener(function(details) {
 	var sendResponse = details.sendResponse;
 	
 	console.assert(sender.id == editThisCookieID || sender.id == swapMyCookiesID || sender.id == forgetMeID);
-    if(request.action != undefined && request.action == "ping") {
-        sendResponse({});
-    }
+	if(request.action != undefined && request.action == "ping") {
+		sendResponse({});
+	}
 });
 */
 
 chrome.extension.onRequestExternal.addListener(function(request, sender, sendResponse) {
-    console.assert(sender.id == editThisCookieID || sender.id == swapMyCookiesID || sender.id == forgetMeID);
-    if(request.action != undefined && request.action == "ping") {
-        sendResponse({});
-    }
+	console.assert(sender.id == editThisCookieID || sender.id == swapMyCookiesID || sender.id == forgetMeID);
+	if(request.action != undefined && request.action == "ping") {
+		sendResponse({});
+	}
 });
 
 
 chrome.extension.onConnectExternal.addListener(function(port){
-    console.log("Connection coming!");
-    console.assert(port.sender.id == editThisCookieID || port.sender.id == swapMyCookiesID || port.sender.id == forgetMeID);
-    port.onMessage.addListener(function(request) {
-        if (request.action != undefined) {
-            if(request.action == "pause") {
-                console.log("Protection paused");
-                PAUSED = true;
-                port.postMessage({
-                    "pause": true
-                });
-            }
-            else if(request.action == "resume") {
-                console.log("Protection resumed");
-                PAUSED = false;
-                port.postMessage({
-                    "resume": true
-                });
-            }
-        }
-    });
+	console.log("Connection coming!");
+	console.assert(port.sender.id == editThisCookieID || port.sender.id == swapMyCookiesID || port.sender.id == forgetMeID);
+	port.onMessage.addListener(function(request) {
+		if (request.action != undefined) {
+			if(request.action == "pause") {
+				console.log("Protection paused");
+				PAUSED = true;
+				port.postMessage({
+					"pause": true
+				});
+			}
+			else if(request.action == "resume") {
+				console.log("Protection resumed");
+				PAUSED = false;
+				port.postMessage({
+					"resume": true
+				});
+			}
+		}
+	});
 });
 
 function setContextMenu(show) {
 	chrome.contextMenus.removeAll();
 	if(show) {
 		chrome.contextMenus.create({
-		    "title":_getMessage("ContextMenu"),
-		    "contexts":["all"],
-		    "onclick":function(info,tab){
-		        showPopup(info,tab);
-		    }
+			"title":_getMessage("ContextMenu"),
+			"contexts":["all"],
+			"onclick":function(info,tab){
+				showPopup(info,tab);
+			}
 		});
 	}
 }
