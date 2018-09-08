74fe2b25b01b7ac2a86fc876420517750229004d
Francesco Capano <fcapano@gmail.com>
2018-09-08 14:45:13 +0100

    Streamline support page

--- options_pages/support.js
+++ options_pages/support.js
@@ -1,18 +1,7 @@
 jQuery(document).ready(function () {
     $("input:checkbox").uniform();
-    setAdsOption();
     setEvents();
-    setDonation();
 });
-
-updateCallback = function () {
-    setAdsOption();
-};
-
-function setAdsOption() {
-    $(':checkbox').removeAttr('checked');
-    $.uniform.update();
-}
 
 function setEvents() {
     $(".linkify").click(function () {
@@ -29,95 +18,4 @@ function setEvents() {
             });
         });
     });
-}
-
-function setDonation() {
-    var slider = $("#paypal-slider").slider({
-        animate: false,
-        value: 15,
-        min: 1,
-        max: 100,
-        step: 1,
-        slide: function (event, ui) {
-            $("#amount-show").html(ui.value);
-            setDonationMessage(ui.value);
-        },
-        create: function (event, ui) {
-            $("#amount-show").html(15);
-            setDonationMessage(15);
-        }
-    });
-
-    //$( "#paypal-donate-button" ).button();
-    $("#paypal-donate-button").click(function () {
-        $("#paypal-form-amount").removeAttr("disabled");
-        submitPayPalForm();
-    });
-
-    //$( "#other-donate-button" ).button();
-    $("#other-donate-button").click(function () {
-        openExtPage("http://www.editthiscookie.com/contribute/");
-    });
-
-    $("#paypal-custom-amount").click(function () {
-        $("#paypal-form-amount").attr("disabled", "disabled");
-        submitPayPalForm();
-    });
-}
-
-function submitPayPalForm() {
-    var lang = "US";
-    if (/IT/i.test(window.navigator.language)) {
-        lang = "IT";
-    } else if (/ES/i.test(window.navigator.language)) {
-        lang = "ES";
-    }
-    //$("#paypal-form-lang").val(lang);
-    var amount = $("#paypal-slider").slider("value");
-    $("#paypal-form-amount").val(amount);
-    var currency = $("#paypal-currency-selector").val();
-    $("#paypal-form-currency").val(currency);
-    $("#paypal-form").submit();
-}
-
-function setDonationMessage(amount) {
-    var benefitItems = $("#benefits-list > li")
-    $("#benefits-list > li").addClass("benefit-unmatched");
-
-    if (amount >= 1)
-        $(benefitItems.get(0)).removeClass("benefit-unmatched");
-    else
-        return;
-    if (amount >= 5)
-        $(benefitItems.get(1)).removeClass("benefit-unmatched");
-    else
-        return;
-    if (amount >= 10)
-        $(benefitItems.get(2)).removeClass("benefit-unmatched");
-    else
-        return;
-    if (amount >= 15)
-        $(benefitItems.get(3)).removeClass("benefit-unmatched");
-    else
-        return;
-    if (amount >= 20)
-        $(benefitItems.get(4)).removeClass("benefit-unmatched");
-    else
-        return;
-    if (amount >= 30)
-        $(benefitItems.get(5)).removeClass("benefit-unmatched");
-    else
-        return;
-    if (amount >= 35)
-        $(benefitItems.get(6)).removeClass("benefit-unmatched");
-    else
-        return;
-    if (amount >= 40)
-        $(benefitItems.get(7)).removeClass("benefit-unmatched");
-    else
-        return;
-    if (amount >= 50)
-        $(benefitItems.get(8)).removeClass("benefit-unmatched");
-    else
-        return;
 }
